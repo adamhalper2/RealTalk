fe8850698119952dd5c65331ecee685fd38ad2cf
Adam Halper <adam.halper@block.one>
2019-04-26 17:00:24 -0700

    Initial Commit

---
+++ RealTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTalk
+//
+//  Created by Adam Halper on 4/26/19.
+//  Copyright © 2019 Adam Halper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
