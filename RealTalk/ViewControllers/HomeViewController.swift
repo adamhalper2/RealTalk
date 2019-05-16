c304fdd7fff0f8b3e5e87692416a51be270dbe92
Adam Halper <adam.halper@block.one>
2019-05-16 15:28:12 -0700

    fixed bug with deleting post from home feed

--- RealTalk/ViewControllers/HomeViewController.swift
+++ RealTalk/ViewControllers/HomeViewController.swift
@@ -381,7 +381,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         let post = posts[indexPath.row]
         if post.authorID == AppController.user?.uid {
             let deleteButton = UITableViewRowAction(style: .default, title: "Delete Chat") { (action, indexPath) in
-                self.tableView.dataSource?.tableView!(self.tableView, commit: .delete, forRowAt: indexPath)
+                self.removePostFromTable(post)
                 return
             }
             deleteButton.backgroundColor = UIColor.red
