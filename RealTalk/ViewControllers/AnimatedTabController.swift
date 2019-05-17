fbae2c426da06de965b52a73ca3000929d9cba1a
Adam Halper <adam.halper@block.one>
2019-05-16 19:40:25 -0700

    tab switching animation

---
+++ RealTalk/ViewControllers/AnimatedTabController.swift
@@ -0,0 +1,68 @@
+//
+//  AnimatedTabController.swift
+//  RealTalk
+//
+//  Created by Adam Halper on 5/16/19.
+//  Copyright © 2019 Adam Halper. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        delegate = self
+    }
+}
+
+extension AnimatedTabController: UITabBarControllerDelegate  {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let tabViewControllers = tabBarController.viewControllers, let toIndex = tabViewControllers.index(of: viewController) else {
+            return false
+        }
+        animateToTab(toIndex: toIndex)
+        return true
+    }
+
+    func animateToTab(toIndex: Int) {
+        guard let tabViewControllers = viewControllers,
+            let selectedVC = selectedViewController else { return }
+
+        guard let fromView = selectedVC.view,
+            let toView = tabViewControllers[toIndex].view,
+            let fromIndex = tabViewControllers.index(of: selectedVC),
+            fromIndex != toIndex else { return }
+
+
+        // Add the toView to the tab bar view
+        fromView.superview?.addSubview(toView)
+
+        // Position toView off screen (to the left/right of fromView)
+        let screenWidth = UIScreen.main.bounds.size.width
+        let scrollRight = toIndex > fromIndex
+        let offset = (scrollRight ? screenWidth : -screenWidth)
+        toView.center = CGPoint(x: fromView.center.x + offset, y: toView.center.y)
+
+        // Disable interaction during animation
+        view.isUserInteractionEnabled = false
+
+        UIView.animate(withDuration: 0.3,
+                       delay: 0.0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseOut,
+                       animations: {
+                        // Slide the views by -offset
+                        fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y)
+                        toView.center = CGPoint(x: toView.center.x - offset, y: toView.center.y)
+
+        }, completion: { finished in
+            // Remove the old view from the tabbar view.
+            fromView.removeFromSuperview()
+            self.selectedIndex = toIndex
+            self.view.isUserInteractionEnabled = true
+        })
+    }
+}
